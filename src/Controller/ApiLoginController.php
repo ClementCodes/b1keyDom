e84b67b2d6363dd2e6ad998a2c7f76f260a0d5cc
DOMINIQUE <clement.dominique@cgi.com>
2023-02-23 10:44:35 +0100

    sauvegarde avant personnalisation de login

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login_check2', name: 'app_api_login',  methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(Request $request, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
-        return $this->json([
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $location =  $urlGenerator->generate('user_get', ['id' => $user->getId()], UrlGenerator::ABSOLUTE_URL);
+        return new JsonResponse([
             'username' => 'mrdomazcleemen@gmail.com',
             'password' => 'password',
+            ["Location" => $location]
         ]);
     }
 }
